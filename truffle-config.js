9a98828f35d9368c25ad1e553d68fc6074b12e53
You Song Hou <yousonghou@gmail.com>
2021-09-16 20:49:32 -0400

    add config to deploy contracts on Harmony Testnet

--- truffle-config.js
+++ truffle-config.js
@@ -1,10 +1,23 @@
+require('dotenv').config();
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const mnemonic = process.env.MNEMONIC;
+
 module.exports = {
   networks: {
     development: {
       host: "127.0.0.1",     // Localhost (default: none)
       port: 7545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
-    }
+    },
+    // truffle migrate --network onetestnet
+    onetestnet: {
+      provider: () => new HDWalletProvider(mnemonic, "https://api.s0.b.hmny.io"),
+      network_id: 1666700000,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
+    },
   },
   contracts_build_directory: './src/abis/',
   compilers: {
